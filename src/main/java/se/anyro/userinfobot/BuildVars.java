893c978c7013e05904070391f89f706e5c81d0a1
Nejamuddin <80298762+Nejamuddin@users.noreply.github.com>
2026-05-15 00:06:17 +0300

    Update BuildVars.java

--- src/main/java/se/anyro/userinfobot/BuildVars.java
+++ src/main/java/se/anyro/userinfobot/BuildVars.java
@@ -1,6 +1,9 @@
 package se.anyro.userinfobot;
 
 public class BuildVars {
-    public static final int OWNER = // Your user id here. Send message to @userinfobot to get it.
-    public static final String TOKEN = // Your bot token here. Use @BotFather.
+
+    public static final long OWNER = 8233649868L;
+
+    public static final String TOKEN = "8819724009:AAHJKSmQakxT8XKOzZxu2zEX-3z8sMWcouA";
+
 }
