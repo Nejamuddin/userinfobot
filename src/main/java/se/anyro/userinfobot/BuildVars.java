3e55f9efb44a0e7b7cbf3077802de316300109c4
Nejamuddin <80298762+Nejamuddin@users.noreply.github.com>
2026-05-15 02:02:55 +0300

    Update BuildVars.java

--- src/main/java/se/anyro/userinfobot/BuildVars.java
+++ src/main/java/se/anyro/userinfobot/BuildVars.java
@@ -1,9 +1,6 @@
 package se.anyro.userinfobot;
 
 public class BuildVars {
-
     public static final long OWNER = 8233649868L;
-
     public static final String TOKEN = "8819724009:AAHJKSmQakxT8XKOzZxu2zEX-3z8sMWcouA";
-
 }
