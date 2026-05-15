e649a046c60975023f6b23cf3c7fc691abb33b77
Nejamuddin <80298762+Nejamuddin@users.noreply.github.com>
2026-05-15 06:51:20 +0300

    Refactor bot credentials and add HTTP server
    Updated bot username and token retrieval to use environment variables. Added HTTP server setup for bot status.

--- src/main/java/se/anyro/userinfobot/UserInfoServlet.java
+++ src/main/java/se/anyro/userinfobot/UserInfoServlet.java
@@ -1,42 +1,59 @@
 package se.anyro.userinfobot;
 
+import com.sun.net.httpserver.HttpServer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.net.InetSocketAddress;
+
 public class UserInfoServlet extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "probashi_info_bot";
+        return System.getenv("BOT_USERNAME");
     }
 
     @Override
     public String getBotToken() {
-        return BuildVars.TOKEN;
+        return System.getenv("BOT_TOKEN");
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage()) {
-            long chatId = update.getMessage().getChatId();
+        try {
+            if (update.hasMessage()) {
+                long chatId = update.getMessage().getChatId();
 
-            SendMessage message = new SendMessage();
-            message.setChatId(String.valueOf(chatId));
-            message.setText("Your ID: " + chatId);
+                SendMessage msg = new SendMessage();
+                msg.setChatId(String.valueOf(chatId));
+                msg.setText("Your ID: " + chatId);
 
-            try {
-                execute(message);
-            } catch (Exception e) {
-                e.printStackTrace();
+                execute(msg);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws Exception {
+        String portText = System.getenv().getOrDefault("PORT", "10000");
+        int port = Integer.parseInt(portText);
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", exchange -> {
+            String response = "Bot is running";
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+        server.start();
+
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         botsApi.registerBot(new UserInfoServlet());
+
+        System.out.println("Bot started on port " + port);
     }
 }
